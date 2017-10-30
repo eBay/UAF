b53f9c3ce6419c8661585eadd9c9fd04a492c2c1
Johannes Jasper <johannes.jasper@nexenio.com>
2017-10-30 17:46:05 +0100

    remove client and server implementations

--- fidouafclient/app/src/main/java/org/ebayopensource/fido/uaf/crypto/FidoAttestationSigner.java
+++
@@ -1,6 +0,0 @@
-package org.ebayopensource.fido.uaf.crypto;
-
-public interface FidoAttestationSigner {
-
-    public byte[] signWithAttestationCert(byte[] dataForSigning);
-}
--- fidouafclient/app/src/main/java/org/ebayopensource/fido/uaf/crypto/FidoSigner.java
+++
@@ -1,9 +0,0 @@
-package org.ebayopensource.fido.uaf.crypto;
-
-import java.security.KeyPair;
-
-public interface FidoSigner {
-
-    public abstract byte[] sign(byte[] dataToSign, KeyPair keyPair);
-
-}
