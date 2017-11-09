ed77408d4175509daf225dc8f846e88b2bca571f
Stephan Schultz <register@steppschuh.net>
2017-11-09 13:23:59 +0100

    Fixed wrong throws declaration

--- fido-uaf-core/src/main/java/com/nexenio/fido/uaf/core/crypto/CertificateValidatorImpl.java
+++ fido-uaf-core/src/main/java/com/nexenio/fido/uaf/core/crypto/CertificateValidatorImpl.java
@@ -14,36 +14,40 @@ public class CertificateValidatorImpl implements CertificateValidator {
     /***
      * Example implementation. It only knows to verify SHA256withEC algorithm.
      */
-    public boolean validate(String cert, String signedData, String signature) throws NoSuchAlgorithmException, IOException, CertificateException, InvalidAlgorithmParameterException, NoSuchProviderException, InvalidKeyException, SignatureException {
+    public boolean validate(String cert, String signedData, String signature) throws CertificateVerificationException {
         byte[] certBytes = Base64.decode(cert);
         byte[] signedDataBytes = Base64.decode(signedData);
         byte[] signatureBytes = Base64.decode(signature);
         return validate(certBytes, signedDataBytes, signatureBytes);
     }
 
-    public boolean validate(byte[] certBytes, byte[] signedDataBytes, byte[] signatureBytes) throws NoSuchAlgorithmException, IOException, CertificateException, NoSuchProviderException, InvalidAlgorithmParameterException, SignatureException, InvalidKeyException {
-        X509Certificate certificate = X509.parseDer(certBytes);
-        String algorithmName = certificate.getSigAlgName();
-        if (algorithmName.contains(RSA.ALGORITHM_RSA)) {
-            return RSA.verify(certificate, signedDataBytes, signatureBytes);
-        }
-
-        byte[] encodedPublicKey = KeyCodec.getKeyAsRawBytes((ECPublicKey) certificate.getPublicKey());
-        byte[] data = SHA.sha(signedDataBytes, SHA.ALGORITHM_SHA_256);
-        BigInteger[] rs;
-        if (signatureBytes.length == 64) {
-            rs = Asn1.transformRawSignature(signatureBytes);
-        } else {
-            rs = Asn1.decodeToBigIntegerArray(signatureBytes);
-        }
-
-        boolean verified;
+    public boolean validate(byte[] certBytes, byte[] signedDataBytes, byte[] signatureBytes) throws CertificateVerificationException {
         try {
-            verified = NamedCurve.verifyUsingSecp256r1(encodedPublicKey, data, rs);
+            X509Certificate certificate = X509.parseDer(certBytes);
+            String algorithmName = certificate.getSigAlgName();
+            if (algorithmName.contains(RSA.ALGORITHM_RSA)) {
+                return RSA.verify(certificate, signedDataBytes, signatureBytes);
+            }
+
+            byte[] encodedPublicKey = KeyCodec.getKeyAsRawBytes((ECPublicKey) certificate.getPublicKey());
+            byte[] data = SHA.sha(signedDataBytes, SHA.ALGORITHM_SHA_256);
+            BigInteger[] rs;
+            if (signatureBytes.length == 64) {
+                rs = Asn1.transformRawSignature(signatureBytes);
+            } else {
+                rs = Asn1.decodeToBigIntegerArray(signatureBytes);
+            }
+
+            boolean verified;
+            try {
+                verified = NamedCurve.verifyUsingSecp256r1(encodedPublicKey, data, rs);
+            } catch (Exception e) {
+                verified = NamedCurve.verifyUsingSecp256k1(encodedPublicKey, data, rs);
+            }
+            return verified;
         } catch (Exception e) {
-            verified = NamedCurve.verifyUsingSecp256k1(encodedPublicKey, data, rs);
+            throw new CertificateVerificationException("Certificate validation failed", e);
         }
-        return verified;
     }
 
 }
